9c69379b8d7165287156d8612361ce53a4efe3e5
Greg Turbo <stkasmir@gmail.com>
2024-08-17 13:47:57 -0700

    Update NesConsole.cpp

--- src/NesConsole.cpp
+++ src/NesConsole.cpp
@@ -38,6 +38,7 @@ NesConsole::NesConsole(uint data_pin, uint clock_pin, uint latch_pin, PIO pio, i
 NesConsole::~NesConsole() {
     nes_device_port_terminate(&_port);
     gpio_set_irq_enabled(_port.latch_pin, GPIO_IRQ_EDGE_RISE, false);
+    gpio_remove_raw_irq_handler(_port.data_pin, &SnesLatchIrqHandler);
     irq_set_enabled(IO_IRQ_BANK0, false);
     _instance = nullptr;
 }
