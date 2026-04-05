422f16f6b2ea91b4da1c90876fa2a8b7f0e0a88a
stephen <stkasmir@gmail.com>
2026-04-04 19:08:04 -0700

    stupid hack

--- src/NesConsole.cpp
+++ src/NesConsole.cpp
@@ -1,4 +1,5 @@
 #include "NesConsole.hpp"
+#include "Arduino.h"
 
 #ifdef CFG_NES_CONFIG_FILE
 #include CFG_NES_CONFIG_FILE
@@ -11,13 +12,26 @@
 #define INVALID_INSTANCE 0xff
 
 static NesConsole *_instance = nullptr;
+static uint8_t data = 0;
+static uint8_t latch = 0;
 
-//not good, fix later
+bool LED_OK = false;
+bool SCREEN_OK = false;
+
+uint64_t counter = 0;
+
+//really not a fan but this hack works
 void __not_in_flash_func(LatchIrqHandler)() {
     gpio_put(8, 1);
     _instance->_report.reserved = 0xFF;
     nes_device_send_packet(&_instance->_port, _instance->_report.raw16);
     gpio_acknowledge_irq(10, GPIO_IRQ_EDGE_RISE);
+    if(counter % 2 == 0) {
+        LED_OK = true;
+    } else {
+        SCREEN_OK = true;
+    }
+    counter += 1;
 }
 
 NesConsole::NesConsole(uint data_pin, uint clock_pin, uint latch_pin, PIO pio, int sm, int offset) {
@@ -26,14 +40,18 @@ NesConsole::NesConsole(uint data_pin, uint clock_pin, uint latch_pin, PIO pio, i
     }
 
     nes_device_port_init(&_port, data_pin, clock_pin, latch_pin, packet_size, pio, sm, offset);
-    gpio_add_raw_irq_handler_with_order_priority(10, &LatchIrqHandler, 255);
+    data = data_pin;
+    latch = latch_pin;
+    gpio_add_raw_irq_handler_with_order_priority(10, &LatchIrqHandler, PICO_HIGHEST_IRQ_PRIORITY);
     gpio_set_irq_enabled(latch_pin, GPIO_IRQ_EDGE_RISE, true);
+    irq_set_priority(IO_IRQ_BANK0, PICO_HIGHEST_IRQ_PRIORITY);
 }
 
 NesConsole::~NesConsole() {
     nes_device_port_terminate(&_port);
     gpio_set_irq_enabled(_port.latch_pin, GPIO_IRQ_EDGE_RISE, false);
     gpio_remove_raw_irq_handler(_port.data_pin, &LatchIrqHandler);
+    //irq_set_enabled(IO_IRQ_BANK0, false);
     _instance = nullptr;
 }
 
@@ -50,3 +68,4 @@ int NesConsole::GetOffset() {
 }
 
 #endif
+
